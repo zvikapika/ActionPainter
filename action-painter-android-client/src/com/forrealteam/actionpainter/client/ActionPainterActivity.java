f00116e367107280e259bfccd740cd5af3b9b4e7
Zvika Markfeld <zakakflemvrid@gmail.com>
2012-03-27 15:46:10 +0200

    update

--- action-painter-android-client/src/com/forrealteam/actionpainter/client/ActionPainterActivity.java
+++ action-painter-android-client/src/com/forrealteam/actionpainter/client/ActionPainterActivity.java
@@ -29,17 +29,21 @@ import android.os.PowerManager.WakeLock;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ActionPainterActivity extends Activity implements SensorEventListener 
 {
+	private static final int SPLASH_THRESHOLD = 5;
 	private static final String TAG = "ACTION_PAINTER";
 	private static final String SERVER_IP = "http://action-painter.appspot.com/ip.jsp";
-	// private static final double RAD = (double) 57.2957795;
+	private static final double RAD = (double) 57.2957795;
 
-	private TextView mTextView, mTextView2;
-	private Socket mSocket; 
+	private TextView text1, text2, text3;
+	private Button showMotionDataButton;
+	private Socket mSocket;
 	private OutputStream out; 
 
 	private SensorManager mSensorManager;
@@ -55,7 +59,14 @@ public class ActionPainterActivity extends Activity implements SensorEventListen
 	private float accX,accY,accZ;
 	boolean splash = false; 
 	
-	private boolean debug = false;
+	float[] mLastAccl = new float[3];
+	float[] mLastMag = new float[3];
+	float[] mRotationMatrix = new float[9];
+			
+	private boolean showMotionData = false;
+	private boolean penDown = false;
+	
+	private long lastSplashTimestamp = System.currentTimeMillis();
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -67,35 +78,57 @@ public class ActionPainterActivity extends Activity implements SensorEventListen
 		cid = Math.abs(telephonyManager.getDeviceId().hashCode());
 		
 		setContentView(R.layout.main);
-		mTextView = (TextView)findViewById(R.id.text);
-		mTextView2 = new TextView(this);
-		LinearLayout ll = (LinearLayout)findViewById(R.id.ll);
-		ll.addView(mTextView2);
+		text1 = (TextView)findViewById(R.id.text1);
+		text2 = (TextView)findViewById(R.id.text2);
+		text3 = (TextView)findViewById(R.id.text3);
+		showMotionDataButton = (Button)findViewById(R.id.showMotionData);
+		showMotionDataButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if(showMotionData) {
+					showMotionDataButton.setText(R.string.show_motion_data);
+					showMotionData = false;
+					text1.setText("");
+					text2.setText("");
+				}
+				else {
+					showMotionDataButton.setText(R.string.hide_motion_data);
+					showMotionData = true;
+				}
+			}
+		});
+		final ImageButton paintToggle = (ImageButton)findViewById(R.id.imageButton1);
+		paintToggle.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				penDown = !penDown;
+			}
+		});
 	}
 
-	public void btnClick(View v) {
-		debug = !debug;
-	}
-	
+
 	private void sendSensorData() 
 	{
 		try
 		{
 			if (null == mSocket  || mSocket.isConnected() == false) {
+				text3.setText("Obtaining local server IP...");
 				String ip = getServerIp();
+				text3.setText("local server: " + ip);
 				mSocket = new Socket(ip, 7890);
 				out = mSocket.getOutputStream();
 				Log.d(TAG,"reconnecting");
 			}
 			JSONObject obj = new JSONObject();
 			obj.put("id", cid);
-			obj.put("ax", digit3.format(threshold(accX,5)));
-			obj.put("ay", digit3.format(threshold(accY,5)));
-			obj.put("az", digit3.format(threshold(accZ,15)));
+			obj.put("acx", digit3.format(threshold(accX,1)));
+			obj.put("acy", digit3.format(threshold(accY,1)));
+			obj.put("acz", digit3.format(threshold(accZ,1)));
 			obj.put("az", digit3.format(azimuth));
 			obj.put("pt", digit3.format(pitch));
 			obj.put("rl", digit3.format(roll));
 			obj.put("sp", splash);
+			obj.put("pd", penDown);
 
 			String str = obj.toString();
 			out.write(toBytes(str.length()));
@@ -160,32 +193,35 @@ public class ActionPainterActivity extends Activity implements SensorEventListen
 	@Override
 	public void onSensorChanged(SensorEvent event)
 	{
-//		if (event.sensor.getType() ==  Sensor.TYPE_MAGNETIC_FIELD) {
-//			mLastMag = event.values;
-//
-//			if (null != mLastAccl)
-//			{
-//				float[]  vals = new float[3];
-//				SensorManager.getRotationMatrix(mRotationMatrix, null, mLastAccl, mLastMag);
-//				SensorManager.getOrientation(mRotationMatrix, vals);
-//
-//				azimuth = vals[0] * RAD;
-//				pitch 	= vals[1] * RAD;
-//				roll	= vals[2] * RAD;
-//				mTextView2.setText("Azimuth is: "+azimuth + " pitch " + pitch + " roll " + roll);
-//				sendSensorData();
-//			}
-//		}
-		if (event.sensor.getType() ==  Sensor.TYPE_ORIENTATION) {
-			azimuth = event.values[0];
-			pitch 	= event.values[1];
-			roll	= event.values[2];
-			if(debug) {
-				mTextView2.setText("Azimuth: "+(int)azimuth + "\nPitch: " + (int)pitch + "\nRoll: " + (int)roll);
+		if (event.sensor.getType() ==  Sensor.TYPE_MAGNETIC_FIELD) {
+			mLastMag = event.values;
+
+			if (null != mLastAccl)
+			{
+				float[]  vals = new float[3];
+				SensorManager.getRotationMatrix(mRotationMatrix, null, mLastAccl, mLastMag);
+				SensorManager.getOrientation(mRotationMatrix, vals);
+
+				azimuth = (float)(vals[0] /* RAD*/);
+				pitch 	= (float)(vals[1] /* RAD*/);
+				roll	= (float)(vals[2] /* RAD*/);
+				if(showMotionData) {
+					text2.setText("azimuth: "+(int)azimuth + ", pitch: " + (int)pitch + ", roll: " + (int)roll);
+				}
 			}
-		}		
+		}
+		
+//		if (event.sensor.getType() ==  Sensor.TYPE_ORIENTATION) {
+//			azimuth = event.values[0];
+//			pitch 	= event.values[1];
+//			roll	= event.values[2];
+//			if(debug) {
+//				text2.setText("Azimuth: "+(int)azimuth + "\nPitch: " + (int)pitch + "\nRoll: " + (int)roll);
+//			}
+//		}		
 		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) 
 		{
+			mLastAccl = event.values;
 			final float alpha = (float) 0.8;
 
 			// Movement
@@ -198,17 +234,24 @@ public class ActionPainterActivity extends Activity implements SensorEventListen
 			accY = event.values[1] - gravity[1];
 			accZ = event.values[2] - gravity[2];
 
-			String text = "acceleration-x: " + accX + "\nacceleration-y: " + accY + "\nacceleration-z: " + accZ;
-			if (!text.equals("") & debug) {
-				mTextView.setText(text);
-				mTextView.invalidate();
+			String text = "acc-x: " + digit3.format(accX) + ", acc-y: " + digit3.format(accY) + ", acc-z: " + digit3.format(accZ);
+			if (showMotionData) {
+				text1.setText(text);
+				text1.invalidate();
 			}
 
 			//Log.d(TAG, "X " + x + " Y " + y + " Z " +z);
-			float accelationSquareRoot = (accX * accX + accY * accY + accZ * accZ) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
-			splash =  (accelationSquareRoot >= 3); // twice the accelation of earth
+			// float accelationSquareRoot = (accX * accX + accY * accY + accZ * accZ) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+			
+			splash =  (System.currentTimeMillis() - lastSplashTimestamp > 1000) && ((Math.abs(accZ) > SPLASH_THRESHOLD) && (Math.abs(accY) < SPLASH_THRESHOLD) && (Math.abs(accX) < SPLASH_THRESHOLD)); // (accelationSquareRoot >= 2); // twice the accelation of earth
+			if(splash) {
+				lastSplashTimestamp = System.currentTimeMillis(); 
+				accX = 0;
+				accY = 0;
+				accZ = 0;
+			}
 			// Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT).show();
-			if (azimuth >= 0) { // orientation was read at least once
+			if (mLastMag != null) { // orientation was read at least once
 				sendSensorData();
 			}
 		}
@@ -246,9 +289,9 @@ public class ActionPainterActivity extends Activity implements SensorEventListen
 			Log.d(TAG, "SENSOR: " + snr.getName());
 		}
 
-		mSensorManager.registerListener(this,
-				mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-				SensorManager.SENSOR_DELAY_UI);
+//		mSensorManager.registerListener(this,
+//				mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+//				SensorManager.SENSOR_DELAY_UI);
 
 		mSensorManager.registerListener(this,
 				mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
